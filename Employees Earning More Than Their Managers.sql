d173f6a630311e4f5cb8f437ab95c580c5441a86
Arihant Jain <arihantjain099@gmail.com>
2020-09-09 02:35:28 +0530

    Update Employees Earning More Than Their Managers.sql

--- Employees Earning More Than Their Managers.sql
+++ Employees Earning More Than Their Managers.sql
@@ -24,4 +24,33 @@ Given the Employee table, write a SQL query that finds out employees who earn mo
 
 
 SELECT e1.Name AS Employee FROM Employee AS e1, Employee AS e2 
-WHERE e1.ManagerId = e2.id AND e1.Salary > e2.Salary
+WHERE e1.ManagerId = e2.id AND e1.Salary > e2.Salary;
+
+
+
+
+
+/*
+
+Self join table looks like
+
+
+Id	Name	Salary	ManagerId	Id	Name	Salary	ManagerId
+1	Joe	70000	3	1	Joe	70000	3
+2	Henry	80000	4	1	Joe	70000	3
+3	Sam	60000		1	Joe	70000	3
+4	Max	90000		1	Joe	70000	3
+1	Joe	70000	3	2	Henry	80000	4
+2	Henry	80000	4	2	Henry	80000	4
+3	Sam	60000		2	Henry	80000	4
+4	Max	90000		2	Henry	80000	4
+1	Joe	70000	3	3	Sam	60000	
+2	Henry	80000	4	3	Sam	60000	
+3	Sam	60000		3	Sam	60000	
+4	Max	90000		3	Sam	60000	
+1	Joe	70000	3	4	Max	90000	
+2	Henry	80000	4	4	Max	90000	
+3	Sam	60000		4	Max	90000	
+4	Max	90000		4	Max	90000
+
+*/
